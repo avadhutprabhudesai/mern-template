8c7c822e631c3a0d2a2c4af6ecc883d234e72795
Avadhut Prabhudesai <avadhutprabhudesai23@gmail.com>
2022-03-03 11:43:48 +0530

    sample fetch from react app.

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    fetch('/api/sample')
+      .then((data) => data.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+
   return (
     <div className="App">
       <h1>This is deployed on Netlify</h1>
