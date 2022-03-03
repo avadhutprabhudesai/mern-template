75a3ede4314e038ddef471027870c0113ce4ac00
Avadhut Prabhudesai <avadhutprabhudesai23@gmail.com>
2022-03-03 10:12:01 +0530

    converted to an expressed app.

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Put all API endpoints under '/api'
+app.get('/api/sample', (req, res) => {
+  // Return them as json
+  res.json({
+    data: 'Hello World',
+  });
+
+  console.log(`Sent ${count} passwords`);
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Express app listening on ${port}`);
